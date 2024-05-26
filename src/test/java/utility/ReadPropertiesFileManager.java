bd45baa15578aa7801cbbf95047dea2151f5700f
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-05-26 14:18:07 +0530

    cucumber

--- src/test/java/utility/ReadPropertiesFileManager.java
+++ src/test/java/utility/ReadPropertiesFileManager.java
@@ -1,5 +1,10 @@
 package utility;
 
 public class ReadPropertiesFileManager {
+    public String getValue(String key){
+        return null;
+    }
+    public void openPropertiesFile(){
 
+    }
 }
