7f59c72924a639d8f8e54e1dd4c07bfb4f551787
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-05-26 14:31:01 +0530

    cucumber

--- src/test/java/utility/ReadPropertiesFileManager.java
+++ src/test/java/utility/ReadPropertiesFileManager.java
@@ -1,10 +1,17 @@
 package utility;
 
-public class ReadPropertiesFileManager {
-    public String getValue(String key){
-        return null;
-    }
-    public void openPropertiesFile(){
+import java.io.*;
+import java.util.Properties;
 
+public class ReadPropertiesFileManager {
+    private Properties properties;
+    private InputStreamReader reader;
+    public String getValue(String key) throws IOException {
+        properties.load(getFileStream());
+        return properties.getProperty(key);
+    }
+    public InputStreamReader getFileStream() throws FileNotFoundException {
+        reader=new InputStreamReader(new FileInputStream(new File(IFileManager.PROP_PATH)));
+        return reader;
     }
 }
