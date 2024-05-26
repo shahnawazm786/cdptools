610be93e53d4f148f888187908c1644aafcc4eb6
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-05-26 21:33:44 +0530

    cucumber

--- src/test/java/utility/ApplicationDriverManager.java
+++ src/test/java/utility/ApplicationDriverManager.java
@@ -1,5 +1,37 @@
 package utility;
 
-public class ApplicationDriverManager {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
+public class ApplicationDriverManager {
+private WebDriver driver;
+private ChromeOptions options;
+
+    /***
+     * Pass the browser name from the properties file
+     * @param browserName
+     * @return
+     */
+    public WebDriver getDriver(EBrowser browserName){
+    switch (browserName){
+        case CHROME:
+            System.out.println("CHROME");
+            break;
+        case FIREFOX:
+            System.out.println("Firefox");
+            break;
+        case EDGE:
+            System.out.println("EDGE");
+            break;
+        case SAFARI:
+            System.out.println("Safari");
+            break;
+        default:
+            System.out.println("CHROME");
+
+    }
+
+
+    return null;
+    }
 }
