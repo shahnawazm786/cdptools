6202188de4dc089816c5d235e66349e71a877268
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-05-26 21:34:08 +0530

    cucumber

--- src/test/java/utility/Test.java
+++ src/test/java/utility/Test.java
@@ -5,5 +5,6 @@ import java.io.IOException;
 public class Test {
     public static void main(String[] args) throws IOException {
         System.out.println(ReadPropertiesFileManager.getValue("system.chrome.options.value"));
+        new ApplicationDriverManager().getDriver(EBrowser.CHROME);
     }
 }
