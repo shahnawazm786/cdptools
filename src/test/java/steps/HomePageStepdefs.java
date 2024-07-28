e51583f5a783ca8bb58cd172619c16f3da4e9470
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-07-28 23:52:43 +0530

    selenium cucumber project

---
+++ src/test/java/steps/HomePageStepdefs.java
@@ -0,0 +1,15 @@
+package steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class HomePageStepdefs {
+    @Given("user launch the site and direct to home page")
+    public void userLaunchTheSiteAndDirectToHomePage() {
+
+    }
+
+    @Then("user validate home page component")
+    public void userValidateHomePageComponent() {
+    }
+}
