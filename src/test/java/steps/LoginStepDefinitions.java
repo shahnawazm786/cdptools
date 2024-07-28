9887a9db954d91ca062c550ee96c96b2afd02749
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-07-28 18:45:43 +0530

    cucumber

--- src/test/java/steps/LoginStepDefinitions.java
+++ src/test/java/steps/LoginStepDefinitions.java
@@ -8,19 +8,22 @@ import io.cucumber.java.en.When;
 public class LoginStepDefinitions {
     @Given("user navigate login page of the application")
     public void userNavigateLoginPageOfTheApplication() {
-
+        System.out.println("Login application");
     }
 
     @And("user enter invalid {string} in user text box")
     public void userEnterInvalidInUserTextBox(String username) {
+        System.out.println("user name"+username);
     }
 
     @And("user enter valid {string} in password text box")
     public void userEnterValidInPasswordTextBox(String password) {
+        System.out.println("Password"+password);
     }
 
     @When("user click on submit button")
     public void userClickOnSubmitButton() {
+        System.out.println("Click on Submit button");
     }
 
     @Then("user redirect to user dashboard")
